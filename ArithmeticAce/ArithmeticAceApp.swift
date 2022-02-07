a02384ebd926b9d7731e22d17e588046914b3096
Claire Melville <cmelville@lcs.on.ca>
2022-02-07 09:05:08 -0500

    Initial Commit

---
+++ ArithmeticAce/ArithmeticAceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArithmeticAceApp.swift
+//  ArithmeticAce
+//
+//  Created by Claire Coding Account on 2022-02-07.
+//
+
+import SwiftUI
+
+@main
+struct ArithmeticAceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
